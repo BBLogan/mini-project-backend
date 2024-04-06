3fb52d28ea40280d72871045c740b551b29c88d7
BexLogan <bek.jayne@gmail.com>
2024-04-06 19:06:02 +0800

    initial commit - have I forgotten to install django? probably - more than likely

---
+++ miniprojectbackend/feedback/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class Feedback (models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    goal = models.IntegerField()
+    image = models.URLField()
+    is_open = models.BooleanField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    owner = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    feedback = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
---
+++ miniprojectbackend/feedback/views.py
@@ -0,0 +1,13 @@
+from rest_framework import APIView
+from rest_framework.response import Response
+from .models import Feedback
+from .serializers import FeedbackSerializer
+
+class FeedbackList(APIView):
+    def get(self, request):
+        feedbacks = Feedback.objects.all()
+        serializer = FeedbackSerializer(feedbacks, many=True)
+        return Response(serializer.data)
+
+    def post(self):
+        pass
